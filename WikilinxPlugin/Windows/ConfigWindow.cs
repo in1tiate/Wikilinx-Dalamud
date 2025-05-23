34e56ceabdea89c9a04702b716b1dcfb931c6e82
in1tiate <32779090+in1tiate@users.noreply.github.com>
2025-05-22 20:58:28 -0500

    cleanup

--- WikilinxPlugin/Windows/ConfigWindow.cs
+++ WikilinxPlugin/Windows/ConfigWindow.cs
@@ -8,10 +8,6 @@ namespace WikilinxPlugin.Windows;
 public class ConfigWindow : Window, IDisposable
 {
     private Configuration Configuration;
-
-    // We give this window a constant ID using ###
-    // This allows for labels being dynamic, like "{FPS Counter}fps###XYZ counter window",
-    // and the window ID will always be "###XYZ counter window" for ImGui
     public ConfigWindow(Plugin plugin) : base("Wikilinx Configs")
     {
         Flags = ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoScrollbar |
